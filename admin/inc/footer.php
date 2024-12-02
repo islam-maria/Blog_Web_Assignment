b12cb035d7092315ad36a025f38240cc98f67566
islam-maria <165696972+islam-maria@users.noreply.github.com>
2024-12-02 23:42:45 +0600

    new

--- admin/inc/footer.php
+++ admin/inc/footer.php
@@ -1,9 +1,5 @@
-
-        <div class="clear">
-        </div>
-    </div>
     <div class="clear">
-    </div>
+        </div>
     <div id="site_info">
         <p>
          &copy; Copyright <a href="http://trainingwithliveproject.com">Training with live project</a>. All Rights Reserved.
